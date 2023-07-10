e72a7fdaef67ef05d71bd385ec7fab3353db8fa4
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-07-09 20:23:08 -0700

    Fix seq lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -3,15 +3,15 @@
 def print_fibonacci(length):
     fib_list = []
     if length <= 0:
-        return fib_list
+        print(fib_list)
     else:
         fib_list.append(0)
         if length == 1:
-            return fib_list
+            print(fib_list)
         else:
             fib_list.append(1)
             i = 2
             while i < length:
                 fib_list.append(fib_list[i - 1] + fib_list[i - 2])
                 i += 1
-        return fib_list
+        print(fib_list)
