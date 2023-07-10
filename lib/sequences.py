a5f4dd2d9b0481ffd62529be0d05a58f7a1c0ac0
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-07-09 20:18:11 -0700

    Sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    if length <= 0:
+        return fib_list
+    else:
+        fib_list.append(0)
+        if length == 1:
+            return fib_list
+        else:
+            fib_list.append(1)
+            i = 2
+            while i < length:
+                fib_list.append(fib_list[i - 1] + fib_list[i - 2])
+                i += 1
+        return fib_list
